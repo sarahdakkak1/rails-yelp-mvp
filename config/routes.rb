40af56b4b32da8615bbb4e28ac112603bf355928
sarah dakkak <sarah.dakkak1@gmail.com>
2020-12-19 16:15:04 -0500

    DszC

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "restaurants", to: "restaurants#index"
-   resources :restaurants, only: [:new, :create] do
+  resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
-  get "restaurants/:id", to: "restaurants#show"
 end
